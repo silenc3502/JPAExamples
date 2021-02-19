ae8f10a88b5a7e8b8bf66a92b76f1b819462cb89
silenc3502 <gcccompil3r@gmail.com>
2021-02-20 00:41:06 +0900

    allocationSize Optimization

---
+++ jpa_basics3/src/main/java/com/example/jpa_basics3/JpaBasics3Application.java
@@ -0,0 +1,13 @@
+package com.example.jpa_basics3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBasics3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaBasics3Application.class, args);
+    }
+
+}
---
+++ jpa_basics3/src/test/java/com/example/jpa_basics3/JpaBasics3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpa_basics3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBasics3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
