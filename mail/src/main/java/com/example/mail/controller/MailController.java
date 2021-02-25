7468e4d8d87f83d854ce2b8130da820eeba209d1
silenc3502 <gcccompil3r@gmail.com>
2021-02-26 01:13:26 +0900

    mail send

---
+++ mail/src/main/java/com/example/mail/controller/MailController.java
@@ -0,0 +1,67 @@
+package com.example.mail.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.mail.*;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+
+@Controller
+public class MailController {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @GetMapping("/mailSend")
+    public static void gmailSend() {
+        String user = ""; // 네이버일 경우 네이버 계정, gmail경우 gmail 계정
+        String password = "";   // 패스워드
+
+        // SMTP 서버 정보를 설정한다.
+        Properties prop = new Properties();
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", 465);
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.ssl.enable", "true");
+        prop.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+
+        Session session = Session.getDefaultInstance(prop, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(user, password);
+            }
+        });
+
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(user));
+
+            //수신자메일주소
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress("silenc3502@naver.com"));
+
+            // Subject
+            message.setSubject("제목을 입력하세요"); //메일 제목을 입력
+
+            // Text
+            message.setText("내용을 입력하세요");    //메일 내용을 입력
+
+            // send the message
+            Transport.send(message); ////전송
+            System.out.println("message sent successfully...");
+        } catch (AddressException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (MessagingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
